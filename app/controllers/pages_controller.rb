3dc5b0afd27a87ba9bac184a9b703d589cb11ce5
Esin Adsoy <esin.adsoy@gmail.com>
2020-11-29 23:50:14 +0300

    will come back to all

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @routes = Route.all
     @route = Route.new
-    @playlist = Playlist.new
-    @route.playlist = @playlist
-    @route.user = current_user
-    @track = Track.new
-    @track.playlist = @playlist
   end
 end
---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,5 @@
+class RoutesController < ApplicationController
+  def create
+    
+  end
+end
