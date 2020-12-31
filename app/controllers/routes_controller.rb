61cb27b61b46db2a658932a9d573d8c9317e65b4
Esin Adsoy <esin.adsoy@gmail.com>
2020-12-31 20:30:48 +0300

    experimenting

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,22 +1,47 @@
 
 class RoutesController < ApplicationController
   skip_before_action :authenticate_user!
+   
   def create
     @start = params[:route][:start]
     @end = params[:route][:end]
-    @route_first = Route.new.get_route(@start, @end)
-    @route = Route.new(route_params)
+    @route = Route.new(get_route(@start, @end))
+    gon.start = @route.route.first
+    gon.end = @route.route.last
     gon.route = @route.route
     gon.duration = @route.duration
     gon.distance = @route.distance
     if @route.save 
-      render :root
+      redirect_to root_path
     end
   end
 
   private
 
-  def route_params
-    params.require(:route).permit(:start, :end, :route, :distance, :duration)
+  def get_route(start_point, end_point)
+    start_point_coor = Geocoder.search(start_point)
+    end_point_coor = Geocoder.search(end_point)
+    Mapbox.access_token = ENV['MAPBOX_API_KEY']
+    get_route = Mapbox::Directions.directions([
+      {
+        "longitude" => start_point_coor.first.data['lon'].to_f,
+        "latitude" => start_point_coor.first.data['lat'].to_f
+      }, {
+        "longitude" => end_point_coor.first.data['long'].to_f,
+        "latitude" => end_point_coor.first.data['lat'].to_f
+      }], "driving", {
+      geometries: "geojson"
+      })
+    route_params = {}
+    route_params[:route] = get_route.first['routes'].first['geometry']['coordinates']
+    route_params[:distance] = get_route.first['routes'].first['distance']
+    route_params[:duration] = get_route.first['routes'].first['duration']
+    # route_params[:start] = start_point_coor
+    # route_params[:end] = end_point_coor
+    route_params
   end
+
+  # def route_params
+  #   params.require(:route).permit(:start, :end, :route, :distance, :duration)
+  # end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,6 @@
 # require "mapbox-sdk"
 class Route < ApplicationRecord
+  
   belongs_to :user
   belongs_to :playlist
   # has_one :start
@@ -9,25 +10,7 @@ class Route < ApplicationRecord
   serialize :end, Array
   serialize :route, Array
 
-  def get_route(start_point, end_point)
-    start_point_coor = Geocoder.search(start_point)
-    end_point_coor = Geocoder.search(end_point)
-    Mapbox.access_token = ENV['MAPBOX_API_KEY']
-    @get_route = Mapbox::Directions.directions([
-      {
-        "longitude" => start_point_coor.first.data['lon'].to_f,
-        "latitude" => start_point_coor.first.data['lat'].to_f
-      }, {
-        "longitude" => end_point_coor.first.data['long'].to_f,
-        "latitude" => end_point_coor.first.data['lat'].to_f
-      }], "driving", {
-      geometries: "geojson"
-      })
   
-    @route = @get_route.first['routes'].first['geometry']['coordinates']
-    @distance = @get_route.first['routes'].first['distance']
-    @duration = @get_route.first['routes'].first['duration']
-  end
 end
 # FIXME 
 # date, check distance, check duration,check user_id,  playlist_id, check start_id, check end_id
