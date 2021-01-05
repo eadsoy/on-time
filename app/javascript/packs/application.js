b1c1c2c758c396f53b7087c351cf04842fd9c537
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-05 22:02:18 +0300

    to be fixed: post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,12 +26,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from "../plugins/init_mapbox.js.erb";
-// import { initAutocomplete } from "../plugins/init_autocomplete";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
-  // initAutocomplete();
 });
 // initMapbox();
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,19 +1,18 @@
 // app/javascript/plugins/init_autocomplete.js
-import places from 'places.js';
+// import places from 'places.js';
 
-const initAutocomplete = () => {
-  const addressInput = document.getElementById('search');
-  if (addressInput) {
-    places({ container: addressInput }).configure({
-      language: 'en'
-    })}
-  const addressInputTwo = document.getElementById('search-end');
-  if (addressInputTwo) {
-    places({ container: addressInputTwo }).configure({
-      language: 'en'
-    })
-  };
-};
+// const initAutocomplete = () => {
+//   const addressInput = document.getElementById('search');
+//   if (addressInput) {
+//     places({ container: addressInput }).configure({
+//       language: 'en'
+//     })}
+//   const addressInputTwo = document.getElementById('search-end');
+//   if (addressInputTwo) {
+//     places({ container: addressInputTwo }).configure({
+//       language: 'en'
+//     })
+//   };
+// };
 
-
-export { initAutocomplete };
+// export { initAutocomplete };
