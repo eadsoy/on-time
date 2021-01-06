0979976c01cabbf04bbbc5c1298dff5da020c9de
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-06 03:44:01 +0300

    bugfix

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -10,24 +10,17 @@ class RoutesController < ApplicationController
     if route_params.present?
       if @route.save!
         flash[:notice] = '🥳 Check your route on the map!'
-        reload_page
+        redirect_to root_path 
       else
-        reload_page
+        redirect_to root_path 
       end 
     else
       flash[:alert] = '🚨 Please enter another route'
-      reload_page
+      redirect_to root_path 
     end
   end
 
   private
-
-  def reload_page
-    respond_to do |format|
-      format.html { redirect_to current_page_url, notice: '🚨 Please enter another route' }
-      format.json { head :no_content }
-    end
-  end
   # def route_params
   #   params.require(:route).permit(:start, :end, :route, :distance, :duration)
   # end
