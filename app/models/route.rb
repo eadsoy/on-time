cd192b68185c2991f63944d81cad93077e87c7b5
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-06 16:14:42 +0300

    added validator to route

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,61 +2,55 @@
 class Route < ApplicationRecord
   belongs_to :user
   belongs_to :playlist
-  # has_one :start
-  # has_one :end
   # store :route, accessors: [ :start, :end ]
   serialize :start, Array
   serialize :end, Array
   serialize :route, Array
-  
 
   def self.validate_points(start_point, end_point)
-    regex_coordinates = '^(\[)[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*,\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)(\])$'
+    regex_coordinates = '^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*,\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$'
     match_start = start_point.match?(regex_coordinates)
     match_end = end_point.match?(regex_coordinates)
-    if match_start && match_end
-      start_lon_lat = [start_point]
-      end_lon_lat = [end_point]
-    elsif match_start
-      start_lon_lat = [start_point]
-      end_point_coor = Geocoder.search(end_point)
-      end_lon_lat = [end_point_coor.first.data['lon'].to_f, end_point_coor.first.data['lat'].to_f]
+
+    case match_start
+    when true
+      @start_lon_lat = start_point.chomp.split(',').map(&:to_f)
+      @start_point_coor = @start_lon_lat
     else
-      start_point_coor = Geocoder.search(start_point)
-      end_point_coor = Geocoder.search(end_point)
-      start_lon_lat = [start_point_coor.first.data['lon'].to_f, start_point_coor.first.data['lat'].to_f]
-      end_lon_lat = [end_point_coor.first.data['lon'].to_f, end_point_coor.first.data['lat'].to_f]
+      @start_point_coor = Geocoder.search(start_point)
+      @start_lon_lat = [@start_point_coor.first.data['lon'].to_f, @start_point_coor.first.data['lat'].to_f]
     end
-    start_lon_lat
-    end_lon_lat
+
+    case match_end
+    when true
+      @end_lon_lat = end_point.chomp.split(',').map(&:to_f)
+      @end_point_coor = @end_lon_lat
+    else
+      @end_point_coor = Geocoder.search(end_point)
+      @end_lon_lat = [@end_point_coor.first.data['lon'].to_f, @end_point_coor.first.data['lat'].to_f]
+    end
   end
 
   def self.get_route(start_point, end_point)
-    # start_point_coor = Geocoder.search(start_point)
-    # end_point_coor = Geocoder.search(end_point)
-    # regex_coordinates = '^(\[)[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*,\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)(\])$'
     validate_points(start_point, end_point)
-    if (start_point_coor.presence != nil && end_point_coor.presence != nil)  
-      # start_lon_lat = [start_point_coor.first.data['lon'].to_f, start_point_coor.first.data['lat'].to_f]
-      # end_lon_lat = [end_point_coor.first.data['lon'].to_f, end_point_coor.first.data['lat'].to_f]
+    if (@start_point_coor.presence != nil && @end_point_coor.presence != nil)  
       Mapbox.access_token = ENV['MAPBOX_API_KEY']
       get_route = Mapbox::Directions.directions([
         {
-          "longitude" => start_lon_lat.first,
-          "latitude" => start_lon_lat.last
+          "longitude" => @start_lon_lat.first,
+          "latitude" => @start_lon_lat.last
         }, {
-          "longitude" => end_lon_lat.first,
-          "latitude" => end_lon_lat.last
+          "longitude" => @end_lon_lat.first,
+          "latitude" => @end_lon_lat.last
         }], "driving", {
         geometries: "geojson"
         })
       route_params = {}
       route_params[:route] = get_route.first['routes'].first['geometry']['coordinates']
-      # p route_params[:route]
       route_params[:distance] = get_route.first['routes'].first['distance']
       route_params[:duration] = get_route.first['routes'].first['duration']
-      route_params[:start] = start_lon_lat
-      route_params[:end] = end_lon_lat
+      route_params[:start] = @start_lon_lat
+      route_params[:end] = @end_lon_lat
       route_params[:user] = User.last
       route_params[:playlist] = Playlist.last
   
