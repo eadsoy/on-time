6e69fb7263d26f41e744aad994065cae3a7be483
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-08 02:44:10 +0300

    sign in with spotify working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,4 +22,36 @@ class PagesController < ApplicationController
       gon.routes = route_all_list
     end
   end
+
+  def callback
+    # url = "https://accounts.spotify.com/authorize"
+    # query_params = {
+    #   client_id: ENV["SPOTIFY_CLIENT_ID"],
+    #   response_type: 'code',
+    #   redirect_uri: 'http://localhost:3000/auth/spotify/callback',
+    #   scope: "user-library-read 
+    #   playlist-read-collaborative
+    #   playlist-modify-private
+    #   user-modify-playback-state
+    #   user-read-private
+    #   user-top-read
+    #   playlist-modify-public",
+    #   show_dialog: true
+    # }
+    # redirect_to "#{url}?#{query_params.to_query}"
+    @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+
+    pp @spotify_user
+    pp @spotify_user.credentials
+    # @spotify_user = RSpotify::User.new(
+    #   {
+    #     'credentials' => {
+    #        "token" => self.credentials["access_token"],
+    #        "refresh_token" => self.credentials["refresh_token"],
+    #        "access_refresh_callback" => callback_proc
+    #     } ,
+    #     'id' => self.credentials["user_id"]
+    #   })
+    render :callback
+  end
 end
