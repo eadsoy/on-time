6eae22b2b8328761aa1d74d4bc4ba32eaaa841af
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-08 23:01:27 +0300

    getting there

--- app/controllers/calbacks_controller.rb
+++ app/controllers/calbacks_controller.rb
@@ -1,6 +1,6 @@
 class CallbacksController <  Devise::OmniauthCallbacksController
   def spotify
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect root_path
+    sign_in_and_redirect root_url
   end
 end
