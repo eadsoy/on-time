841f12dc7eb7f5252263a872b9247f8d128ebc0c
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-08 15:20:03 +0300

    devise + omniauth problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 require 'pp'
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :callback]
  
   def home
     respond_to do |format|
